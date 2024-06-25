e5f7b130e428f19b9f7343173252379df95b9ee1
Sakthi2023 <sakthinaveen55@gmail.com>
2024-06-25 09:32:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <form>
+        <label>
+          Name:
+          <input type="text" />
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      <h2>Hello! Bala Priya</h2>
+    </div>
+  );
+}
+
+export default App;
