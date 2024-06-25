4ba705b920ab19d104536b3ae645d01e1ebcdf21
BalaPriyaMS <balapriya@intellicar.in>
2024-06-25 11:43:37 +0530

    updating the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [Inputname, setInputName] = useState("Priya");
+  const [names, setNames] = useState("");
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    setNames(Inputname);
+  };
   return (
     <div>
       <form>
         <label>
           Name:
-          <input type="text" />
+          <input
+            type="text"
+            onChange={(e) => {
+              setInputName(e.target.value);
+            }}
+          />
         </label>
-        <button type="submit">Submit</button>
+        <button onClick={handleClick} type="submit">
+          Submit
+        </button>
       </form>
-      <h2>Hello! Bala Priya</h2>
+      <h2>Hello! {names}</h2>
     </div>
   );
 }
